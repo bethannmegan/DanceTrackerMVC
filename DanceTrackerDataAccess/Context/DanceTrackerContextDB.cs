2d47214cead7deb362cb4f2f49802b6b83d5b17c
Bethann Gold <bethanngold@yahoo.com>
2024-03-27 00:13:59 -0400

    Add project files.

---
+++ DanceTrackerDataAccess/Context/DanceTrackerContextDB.cs
@@ -0,0 +1,40 @@
+﻿using DanceTrackerDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanceTrackerDataAccess.Context
+{
+    public class DanceTrackerContextDB : DbContext
+    {
+        public DanceTrackerContextDB(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected DanceTrackerContextDB()
+        {
+        }
+
+        public DbSet<HomeTable> HomeTable {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HomeTable>(
+                entity =>
+                {
+                    entity.HasKey(e => e.EventId);
+                    entity.Property(e => e.EventId);
+                    entity.Property(e => e.DateOfEvent);
+                    entity.Property(e => e.EventName);
+                    entity.Property(e => e.EventLocation);
+                    entity.Property(e => e.Competitions);
+                    entity.Property(e => e.LessonsAndWorkshops);
+                }
+                
+                );
+        }
+    }
+}
---
+++ DanceTrackerDataAccess/Models/HomeTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanceTrackerDataAccess.Models 
+{
+    public class HomeTable
+    {
+        public int EventId {get; set;}
+
+        public DateTime DateOfEvent { get; set; }
+
+        public string? EventName { get; set; }
+
+        public string? EventLocation { get; set; }
+
+        public string? Competitions { get; set; }
+
+        public string? LessonsAndWorkshops { get; set; }
+
+
+    }
+}
